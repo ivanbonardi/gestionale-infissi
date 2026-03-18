4dfc818761fc15bad12a7f2110c65a90d005a3c7
ivanbonardi <ivan.bonardi@gmail.com>
2026-03-18 14:33:20 +0100

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,10 +1,7 @@
-"use client";
-
-export default function Home() {
+export default function RootLayout({ children }) {
   return (
-    <div style={{ padding: 20 }}>
-      <h1>Gestionale Infissi</h1>
-      <p>App online funzionante 🚀</p>
-    </div>
+    <html lang="it">
+      <body>{children}</body>
+    </html>
   );
 }
