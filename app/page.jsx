aeff7b9fa33e03716912721ad50c9d98d95e9dfe
ivanbonardi <ivan.bonardi@gmail.com>
2026-03-23 22:29:53 +0100

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -61,7 +61,7 @@ export default function Home() {
     ]);
 
     if (error) {
-      alert("Errore salvataggio");
+  alert(error.message);
     } else {
       alert("Salvato!");
       setCliente("");
