b0bdac24729b9e148fb252565de93f5b84b4bb87
ivanbonardi <ivan.bonardi@gmail.com>
2026-03-24 08:23:39 +0100

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,16 @@
+export const metadata = {
+  title: "Gestione Rapportini",
+  description: "App rapportini",
+  manifest: "/manifest.json",
+};
+
 export default function RootLayout({ children }) {
   return (
     <html lang="it">
+      <head>
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#007aff" />
+      </head>
       <body>{children}</body>
     </html>
   );
