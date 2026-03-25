dfef3b77a10586b3a768a2562e1f4c5f10d84f05
ivanbonardi <ivan.bonardi@gmail.com>
2026-03-25 21:33:35 +0100

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -11,6 +11,11 @@ export default function RootLayout({ children }) {
         <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#000000" />
         <link rel="apple-touch-icon" href="/icon-192.png" />
+        <link rel="apple-touch-startup-image" href="/icon-512.png" />
+<meta name="apple-mobile-web-app-capable" content="yes" />
+<meta name="apple-mobile-web-app-status-bar-style" content="default" />
+ <meta name="apple-mobile-web-app-title" content="Rapportini" /> 
+   <meta name="viewport" content="width=device-width, initial-scale=1" />     
       </head>
       <body>{children}</body>
     </html>
